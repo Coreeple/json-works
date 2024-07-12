58439cbd8ee4578fd3dbb6afe6c1b1fc6d63fdb8
kmg7 <kmlgkcy.dev@gmail.com>
2024-07-12 17:04:52 +0300

    create: models with json attributes

---
+++ json-ph/Model/Photo.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Model
+{
+    public class Photo(int albumId, int id, string title, string url, string thumbnailUrl)
+    {
+        [JsonPropertyName("albumId")]
+        public int AlbumId { get; set; } = albumId;
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; } = id;
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; } = title;
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; } = url;
+
+        [JsonPropertyName("thumbnailUrl")]
+        public string ThumbnailUrl { get; set; } = thumbnailUrl;
+    }
+
+
+}
