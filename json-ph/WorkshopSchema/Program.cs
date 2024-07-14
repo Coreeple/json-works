822a16fab0f3897c7e931789df162c8f81e91454
kmg7 <kmlgkcy.dev@gmail.com>
2024-07-14 14:20:53 +0300

    feat: schema evaluation examples

--- json-ph/WorkshopSchema/Program.cs
+++ json-ph/WorkshopSchema/Program.cs
@@ -2,6 +2,10 @@
 
 Console.WriteLine("Workshop for JsonSchema\n\n");
 
-// Generator Schema Examples suitable for basic usage
-GeneratorSchemaExamples.PrintSchemas();
-GeneratorSchemaExamples.ValidationExample();
+//// Generator Schema Examples suitable for basic usage
+//GeneratorSchemaExamples.PrintSchemas();
+//GeneratorSchemaExamples.ValidationExample();
+
+// Schema Examples suitable for advanced usage
+SchemaExamples.PrintSchemas();
+SchemaExamples.ValidationExample("User", "user.alt.json");
---
+++ json-ph/WorkshopSchema/SchemaExamples.cs
@@ -0,0 +1,56 @@
+﻿using Json.Schema;
+using SchemaLib;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace WorkshopSchema
+{
+    public static class SchemaExamples
+    {
+        static List<JsonSchema> _schemas = [.. SchemaProviderFile.GetJsonSchemas(@"../../../../Model/Schemas/", "*.json")];
+        public static void PrintSchemas()
+        {
+
+            Console.WriteLine("SCHEMAS EXAMPLE");
+            Console.WriteLine("Schemas are easy to maintain, they have all the keywords like $id $schema and such.\nTheir uri is editable so it will be easy to distinguish and refer a schema\n");
+            foreach (var schema in _schemas)
+            {
+                Console.WriteLine($"Schema: Type: {schema.GetJsonType()}\nUri: {schema.BaseUri}\n$id: {schema.GetId()}\n");
+            }
+        }
+
+        public static void ValidationExample(string exampleFolder, string modelName)
+        {
+            var options = new EvaluationOptions();
+            var schema = _schemas.First(s => $"{s.GetId()}" == "https://github.com/Coreeple/json-works/json-ph/Model/Schemas/" + modelName);
+            var exampleFiles = Directory.GetFiles(@$"../../../../examples/{exampleFolder}/", "*.json");
+            foreach (var example in exampleFiles)
+            {
+                var fileContent = File.ReadAllText(example);
+                options.OutputFormat = OutputFormat.List;
+                var result = schema!.Evaluate(JsonNode.Parse(fileContent), options: options);
+                PrintResult(result);
+            }
+
+        }
+
+        public static void PrintResult(EvaluationResults? results)
+        {
+            Console.WriteLine(string.Concat(Enumerable.Repeat('\n',3)));
+            if (results is null)
+            {
+                return;
+            }
+            Console.WriteLine(string.Concat(Enumerable.Repeat('#', 40)));
+            if (results.IsValid)
+            {
+                Console.WriteLine("The data is valid");
+                Console.WriteLine(string.Concat(Enumerable.Repeat('#', 40)));
+                return;
+            }
+            var details = JsonSerializer.Serialize(results.Details.Where(d => !d.IsValid), new JsonSerializerOptions { WriteIndented = true });
+            Console.WriteLine(details);
+            Console.WriteLine(string.Concat(Enumerable.Repeat('#', 40)));
+        }
+    }
+}
