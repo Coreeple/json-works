8cbd41f696028fa005f250b9d1ead39456215123
kmg7 <kmlgkcy.dev@gmail.com>
2024-07-14 12:55:22 +0300

    create: providing schemas from files

---
+++ json-ph/SchemaLib/SchemaProviderFile.cs
@@ -0,0 +1,17 @@
+﻿using Json.Schema;
+
+namespace SchemaLib
+{
+    public static class SchemaProviderFile
+    {
+        public static IEnumerable<JsonSchema> GetJsonSchemas(string path, string searchPattern)
+        {
+            var schemaFiles = Directory.GetFiles(path, searchPattern);
+            foreach (var schemaFile in schemaFiles)
+            {
+                var schema = JsonSchema.FromFile(schemaFile);
+                yield return schema;
+            }
+        }
+    }
+}
