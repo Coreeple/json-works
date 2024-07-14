94825f17a2f1eacd08b0742a7b33a5660f40bbdc
kmg7 <kmlgkcy.dev@gmail.com>
2024-07-14 11:00:36 +0300

    create: schema generation example

---
+++ json-ph/SchemaLib/SchemaProviderGenerated.cs
@@ -0,0 +1,27 @@
+﻿using Json.Schema;
+using Json.Schema.Generation;
+using Model;
+
+namespace SchemaLib
+{
+    public static class SchemaProviderGenerated
+    {
+        public static IEnumerable<JsonSchema> GetJsonSchemas()
+        {
+            var schemaBuilder = new JsonSchemaBuilder();
+            List<JsonSchema> schemas = [
+               schemaBuilder.FromType<Geo>().Build(),
+               schemaBuilder.FromType<Address>().Build(),
+               schemaBuilder.FromType<Company>().Build(),
+               schemaBuilder.FromType<User>().Build(),
+               schemaBuilder.FromType<Album>().Build(),
+               schemaBuilder.FromType<Comment>().Build(),
+               schemaBuilder.FromType<Photo>().Build(),
+               schemaBuilder.FromType<Post>().Build(),
+               schemaBuilder.FromType<Todo>().Build(),
+                ];
+
+            return schemas;
+        }
+    }
+}
