4739a341e5397ff0a3e743cbfdb618fee2c3e8a1
kmg7 <kmlgkcy.dev@gmail.com>
2024-07-14 12:34:10 +0300

    update: added id keyword for generated schemas

--- json-ph/SchemaLib/SchemaProviderGenerated.cs
+++ json-ph/SchemaLib/SchemaProviderGenerated.cs
@@ -6,19 +6,19 @@ namespace SchemaLib
 {
     public static class SchemaProviderGenerated
     {
-        public static IEnumerable<JsonSchema> GetJsonSchemas()
+        public static IEnumerable<JsonSchema> GetJsonSchemas(string idPrefix)
         {
             var schemaBuilder = new JsonSchemaBuilder();
             List<JsonSchema> schemas = [
-               schemaBuilder.FromType<Geo>().Build(),
-               schemaBuilder.FromType<Address>().Build(),
-               schemaBuilder.FromType<Company>().Build(),
-               schemaBuilder.FromType<User>().Build(),
-               schemaBuilder.FromType<Album>().Build(),
-               schemaBuilder.FromType<Comment>().Build(),
-               schemaBuilder.FromType<Photo>().Build(),
-               schemaBuilder.FromType<Post>().Build(),
-               schemaBuilder.FromType<Todo>().Build(),
+               schemaBuilder.FromType<Geo>().Id($"{idPrefix}/{typeof(Geo).FullName}").Build(),
+               schemaBuilder.FromType<Address>().Id($"{idPrefix}/{typeof(Address).FullName}").Build(),
+               schemaBuilder.FromType<Company>().Id($"{idPrefix}/{typeof(Company).FullName}").Build(),
+               schemaBuilder.FromType<User>().Id($"{idPrefix}/{typeof(User).FullName}").Build(),
+               schemaBuilder.FromType<Album>().Id($"{idPrefix}/{typeof(Album).FullName}").Build(),
+               schemaBuilder.FromType<Comment>().Id($"{idPrefix}/{typeof(Comment).FullName}").Build(),
+               schemaBuilder.FromType<Photo>().Id($"{idPrefix}/{typeof(Photo).FullName}").Build(),
+               schemaBuilder.FromType<Post>().Id($"{idPrefix}/{typeof(Post).FullName}").Build(),
+               schemaBuilder.FromType<Todo>().Id($"{idPrefix}/{typeof(Todo).FullName}").Build(),
                 ];
 
             return schemas;
