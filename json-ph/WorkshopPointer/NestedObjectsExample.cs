7d2e088f86b5ceeb2e7cda5a685888ae59e8dfdb
kmg7 <kmlgkcy.dev@gmail.com>
2024-07-15 10:54:28 +0300

    add: nested objects example

---
+++ json-ph/WorkshopPointer/NestedObjectsExample.cs
@@ -0,0 +1,58 @@
+﻿using Json.Pointer;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace WorkshopPointer
+{
+    internal class NestedObjectsExample
+    {
+        private JsonNode JsonDoc { get; set; }
+        private JsonSerializerOptions Jsopt { get; set; }
+        private JsonPointer FooBazP { get; } = JsonPointer.Parse("/foo/1");
+        private JsonPointer NestedP { get; } = JsonPointer.Parse("/highly/nested");
+        public NestedObjectsExample(string filePath)
+        {
+            var doc = File.ReadAllText(filePath);
+            JsonDoc = JsonNode.Parse(doc);
+
+            Jsopt = new JsonSerializerOptions();
+            Jsopt.WriteIndented = true;
+
+            Console.WriteLine(JsonDoc.ToJsonString(Jsopt));
+        }
+
+        public void FooBaz(string relativePointer)
+        {
+            var pointer = RelativeJsonPointer.Parse(relativePointer);
+            string value = "null";
+
+            if (pointer is not null && FooBazP.TryEvaluate(JsonDoc, out var fooBaz)
+                && fooBaz is not null)
+            {
+                if (pointer.TryEvaluate(fooBaz, out JsonNode? node) && node is not null) {
+                    value = node.ToJsonString(Jsopt); 
+                };
+            }
+            Console.WriteLine($"Pointer: {relativePointer}\t{value}");
+        }
+
+        public void Nested(string relativePointer)
+        {
+            var pointer = RelativeJsonPointer.Parse(relativePointer);
+            string value = "null";
+
+            if (pointer is not null && NestedP.TryEvaluate(JsonDoc, out var fooBaz)
+                && fooBaz is not null)
+            {
+                if (pointer.TryEvaluate(fooBaz, out JsonNode? node) && node is not null) {
+                    value = node.ToJsonString(Jsopt); 
+                };
+            }
+            Console.WriteLine($"Pointer: {relativePointer}\t{value}");
+        }
+        public void PrintDoc()
+        {
+            Console.WriteLine(JsonDoc.ToJsonString(Jsopt));
+        }
+    }
+}
--- json-ph/WorkshopPointer/Program.cs
+++ json-ph/WorkshopPointer/Program.cs
@@ -14,4 +14,22 @@ bsx.FilterBooksByPrice(10.0);
 bsx.FilterBooksByPrice(20.0);
 bsx.RelativeFilterBooksByPrice(20.0);
 
+// borrowed from https://json-schema.org/draft/2020-12/relative-json-pointer#rfc.section.5.1
+var nestedJsonFP = @"../../../../Examples/Pointer/nested-objects.json";
+var nsx = new NestedObjectsExample(nestedJsonFP);
+
+
+string[] pointersFromFooBaz = ["0", "1/0", "0", "1/0", "0-1", "2/highly/nested/objects", "0#", "0-1#", "1#"];
+nsx.PrintDoc();
+Console.WriteLine("\nStarting from the value 'baz' (inside 'foo')\n");
+foreach (var p in pointersFromFooBaz)
+{
+    nsx.FooBaz(p);
+}
+
+nsx.PrintDoc();
+List<string> pointersFromNestedObj = ["0/objects", "1/nested/objects", "2/foo/0", "0#", "1#"];
+Console.WriteLine(@"Starting from the value {""objects"":true} (corresponding to the member key ""nested"")");
+
+pointersFromNestedObj.ForEach(p => nsx.Nested(p));
 
